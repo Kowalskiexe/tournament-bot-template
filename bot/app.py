eaf064abf40f9a4f8526f1c844accb78b84280c4
Maciej Kowalski <kowalski.exe@gmail.com>
2024-01-05 18:48:58 +0100

    fix typos

--- bot/app.py
+++ bot/app.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def play():
     body = json.loads(request.data)
-    oponents_move = body['oponents_last_move']
-    return json.dumps({"my_move": strategy.play(oponents_move)})
+    opponents_move = body['opponents_last_move']
+    return json.dumps({"my_move": strategy.play(opponents_move)})
 
--- bot/strategy.py
+++ bot/strategy.py
@@ -1,13 +1,13 @@
-# oponents_move is ether 'stay silent' or 'testify'
-def play(oponents_last_move: str) -> str:
+# opponents_last_move is ether 'stay silent', 'testify', 'none' if it's the first round
+def play(opponents_last_move: str) -> str:
     # here implement your strategy
     # example strategy is to stay silent unless the other player testifies
-    if oponents_last_move == 'none':
+    if opponents_last_move == 'none':
         # this is the first move
         return 'stay silent'
 
-    if oponents_last_move == 'stay silent':
+    if opponents_last_move == 'stay silent':
         return 'stay silent'
 
-    # by elimination oponents_last_move must be 'testify'
+    # by elimination opponents_last_move must be 'testify'
     return 'testify'
